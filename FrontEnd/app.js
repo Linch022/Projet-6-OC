1021fbfdde4ba913fb735b5db5c62c0bad934c1c
Nicolas Berthollet <berthollet.nicolas@gmail.com>
2024-11-26 17:28:17 +0100

    create function to display works in html

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -7,14 +7,30 @@ async function fetchData() {
     const data = await response.json();
     worksData = data;
     worksDatalength = data.length
-    
-    
+        
 }
 
 async function displayGallery() {
     await fetchData();
     console.log(worksData);
     console.log(worksDatalength);
+
+    for (i = 0 ; i < worksDatalength; i++) {
+        const imageUrl = worksData[i].imageUrl;
+        const title = worksData[i].title;
+
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        const figCaption = document.createElement("figcaption");
+
+        img.src = imageUrl;
+        img.alt = title;
+        figCaption.textContent = title;
+        
+        figure.appendChild(img);
+        figure.appendChild(figCaption);
+        galleryContainer.appendChild(figure);
+    }
     
 
 }

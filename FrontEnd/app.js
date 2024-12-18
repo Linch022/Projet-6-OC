e39356035fcf739d2141c95a951751d3415d5b93
Nicolas Berthollet <berthollet.nicolas@gmail.com>
2024-12-18 10:53:20 +0100

    Two modal interfaces completed in css and html + display and closing of modals in js

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -94,3 +94,44 @@ async function displayGallery() {
   }
 }
 displayGallery();
+
+
+
+document.querySelector('#portfolio .admin-works-button').addEventListener("click", () => {
+  displayModal();
+})
+
+function displayModal() {
+  const modal = document.getElementById("modal");
+  const overlay = document.getElementById("modal-overlay");
+  const galleryModal = document.querySelector("#modal-gallery-container .modal-gallery");
+  modal.classList.add("modal-open");
+  overlay.classList.add("modal-open")
+  modal.ariaHidden = false;
+  for (i = 0 ; i < worksData.length; i++) {
+      const imageUrl = worksData[i].imageUrl;
+      const figure = document.createElement("figure");
+      const img = document.createElement("img");
+      const icone = document.createElement("i");
+      icone.classList.add("fa-solid", "fa-trash-can");
+      img.src = imageUrl;
+      figure.appendChild(icone);
+      figure.appendChild(img);
+      galleryModal.appendChild(figure);
+  }
+}
+
+const photoInput = document.getElementById("photo-input");
+
+photoInput.addEventListener("change", (e) => {
+    console.log(e.target.files[0]);
+    
+})
+
+document.querySelectorAll(".close-modal").forEach((element) => {
+  element.addEventListener("click", () => {
+    document.querySelectorAll(".modal-open").forEach((item) => {
+      item.classList.remove("modal-open");
+    })
+  })
+})

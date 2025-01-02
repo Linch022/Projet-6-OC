e3c445ec405b1328514449c95204f1de57a2e4d0
Nicolas Berthollet <berthollet.nicolas@gmail.com>
2025-01-02 16:16:25 +0100

    Check the model and add comments

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -2,42 +2,37 @@ const WORKSAPI = "http://localhost:5678/api/works";
 const CATEGORIESAPI = "http://localhost:5678/api/categories";
 let userToken = window.sessionStorage.getItem("token");
 const adminOption = document.querySelectorAll(".admin-option");
-let worksData;
-let categoriesData;
+const inputs = document.querySelectorAll(".upload-inputs");
+const photoInput = document.getElementById("photo-input");
+let worksData = [];
+let categoriesData = [];
 
+// Variable qui contient un objet Set qui permettra de filter les Works
 const filters = new Set();
 
+
+// Fonction de Fetch pour les appels API avec une méthode de base en GET et la possibilité de passer les autres méthodes en paramètre
 async function fetchData(
   url,
   { method = "GET", headers = {}, body = null } = {}
 ) {
   try {
-    console.log(url);
-    console.log(method);
-    console.log(headers);
-
     const options = {
       method,
       headers,
     };
-
     if (body) {
       options.body = body;
     }
-
     const response = await fetch(url, options);
-
     if (!response.ok) {
       console.error(`Erreur HTTP : ${response.status} ${response.statusText}`);
       return null;
     }
-
     try {
       const data = await response.json();
-      console.log("Réponse JSON :", data);
       return data;
     } catch (error) {
-      console.log("Réponse sans JSON.");
       return response;
     }
   } catch (error) {
@@ -46,6 +41,7 @@ async function fetchData(
   }
 }
 
+//Permet de vérifier si un utilisateur est connecté au lancement de la page
 function isLog() {
   if (userToken) {
     adminOption.forEach((e) => e.classList.add("admin-mode"));
@@ -53,12 +49,48 @@ function isLog() {
 }
 isLog();
 
+// Permet de déconnecter l'utilisateur au clique sur le bouton logout
 document.getElementById("logout-button").addEventListener("click", () => {
   window.sessionStorage.removeItem("token");
   userToken = null;
   adminOption.forEach((e) => e.classList.remove("admin-mode"));
 });
 
+// Fonction qui permet de créer un filtres
+function createFilter(filter) {
+  filters.add(filter.id);
+  const li = document.createElement("li");
+  const button = document.createElement("button");
+  button.textContent = filter.name;
+  button.id = `categoryId-${filter.id}`;
+  button.classList =
+    filter.id === 0 ? "filter-button active-filter" : "filter-button";
+  button.addEventListener("click", () => {
+    changeFilter(button, filter.id);
+  });
+  li.appendChild(button);
+  document.getElementById("filters-container").appendChild(li);
+}
+
+// Fonction appelée avec l'event listener sur les filtres pour filtrer la gallerie 
+function changeFilter(button, filterId) {
+  document
+    .querySelector("#portfolio .active-filter")
+    .classList.remove("active-filter");
+  button.classList.add("active-filter");
+  filters.clear();
+  if (filterId === 0) {
+    filters.add(0);
+    categoriesData.forEach((item) => {
+      filters.add(item.id);
+    });
+  } else {
+    filters.add(filterId);
+  }
+  displayGallery();
+}
+
+// Récupères les données de l'api catégories puis lance la fonction pour créer un filtre
 async function displayCategories() {
   categoriesData = await fetchData(CATEGORIESAPI);
   const baseFilter = { name: "Tous", id: 0 };
@@ -66,16 +98,20 @@ async function displayCategories() {
   categoriesData.map((item) => {
     createFilter(item);
   });
-  console.log(filters);
 }
-
 displayCategories();
+
+
+// Récupère les données Works dans l'api
+async function fetchGalleryData() {
+  worksData = await fetchData(WORKSAPI);
+}
 
 //Fonction permettant l'injection des works dans le html
 async function displayGallery() {
   const galleryContainer = document.querySelector("#portfolio .gallery");
-  worksData = await fetchData(WORKSAPI);
-  galleryContainer.replaceChildren();
+  galleryContainer.replaceChildren(); // Permet de vider le container de la gallerie avant de générer la gallerie
+    // Boucle pour la création des items dans la gallerie
   for (let i = 0; i < worksData.length; i++) {
     if (filters.has(worksData[i].categoryId)) {
       const imageUrl = worksData[i].imageUrl;
@@ -95,46 +131,26 @@ async function displayGallery() {
     }
   }
 }
-displayGallery();
 
-function createFilter(filter) {
-  filters.add(filter.id);
-  const li = document.createElement("li");
-  const button = document.createElement("button");
-  button.textContent = filter.name;
-  button.id = `categoryId-${filter.id}`;
-  button.classList =
-    filter.id === 0 ? "filter-button active-filter" : "filter-button";
-  button.addEventListener("click", () => {
-    changeFilter(button, filter.id);
-  });
-  li.appendChild(button);
-  document.getElementById("filters-container").appendChild(li);
+// Lance les fonctions d'appel à l'api puis d'affichage des éléments de la gallerie
+async function initGallery() {
+  await fetchGalleryData();
+  await displayGallery();
 }
+document.addEventListener("DOMContentLoaded", async () => {
+  await initGallery();
+});
 
-function changeFilter(button, filterId) {
-  document
-    .querySelector("#portfolio .active-filter")
-    .classList.remove("active-filter");
-  button.classList.add("active-filter");
-  filters.clear();
-  if (filterId === 0) {
-    filters.add(0);
-    categoriesData.forEach((item) => {
-      filters.add(item.id);
-    });
-  } else {
-    filters.add(filterId);
-  }
-  displayGallery();
-}
 
+// EventListener pour lancer la fonction d'affichage de la modale
 document
   .querySelector("#portfolio .admin-works-button")
   .addEventListener("click", () => {
     displayModal();
   });
 
+
+//Fonction permettant l'affichage de la modale
 function displayModal() {
   const modal = document.getElementById("modal");
   const galleryModal = document.querySelector(
@@ -143,13 +159,15 @@ function displayModal() {
   modal.classList.add("modal-open");
   document.getElementById("modal-overlay").classList.add("modal-open");
   modal.ariaHidden = false;
-  galleryModal.replaceChildren();
-  document.getElementById("photo-category").replaceChildren();
+  galleryModal.replaceChildren(); // Permet de vider le container de la gallerie avant de générer la gallerie
+  document.getElementById("photo-category").replaceChildren(); // Vide l'input select du formulaire avant de le générer
+  // Boucle pour la création des items dans la gallerie
   for (let i = 0; i < worksData.length; i++) {
     const figure = document.createElement("figure");
     const img = document.createElement("img");
     const icone = document.createElement("i");
     icone.classList.add("fa-solid", "fa-trash-can");
+    // Event Listener sur la poubelle qui permet de supprimer un Works
     icone.addEventListener("click", () => {
       fetchData(`${WORKSAPI}/${worksData[i].id}`, {
         method: "DELETE",
@@ -160,12 +178,14 @@ function displayModal() {
       });
       worksData = worksData.filter((work) => work.id !== worksData[i].id);
       displayModal();
+      displayGallery();
     });
     img.src = worksData[i].imageUrl;
     figure.appendChild(icone);
     figure.appendChild(img);
     galleryModal.appendChild(figure);
   }
+  // Boucle pour la création des éléments de l'input select
   for (let j = 0; j < categoriesData.length; j++) {
     const option = document.createElement("option");
     option.value = categoriesData[j].name;
@@ -176,8 +196,7 @@ function displayModal() {
   console.log(worksData);
 }
 
-const photoInput = document.getElementById("photo-input");
-
+// Permet de vérifier si l'image uploadée dans le formulaire est conforme puis ajoute une miniature
 photoInput.addEventListener("change", (e) => {
   console.log(e.target.files[0]);
   const preview = document.getElementById("image-preview");
@@ -197,14 +216,18 @@ photoInput.addEventListener("change", (e) => {
   }
 });
 
+// Permet de fermer la modale
 document.querySelectorAll(".close-modal").forEach((e) => {
   e.addEventListener("click", () => {
+    const modal = document.getElementById("modal");
+    modal.ariaHidden = true;
     document.querySelectorAll(".modal-open").forEach((item) => {
       item.classList.remove("modal-open");
     });
   });
 });
 
+// Les events Listeners pour passer d'une modale à l'autre
 document.getElementById("open-upload-modal").addEventListener("click", () => {
   document
     .getElementById("modal-gallery-container")
@@ -219,16 +242,19 @@ document.getElementById("modal-back-btn").addEventListener("click", () => {
   document.getElementById("modal-photo-upload").classList.remove("upload-open");
 });
 
-const inputs = document.querySelectorAll(".upload-inputs");
+// Permet de vérifier que les inputs soient bien remplis dans le formualaire puis d'enlever le disable du bouton submit
 function checkInputs() {
   const allFilled = Array.from(inputs).every((input) => input.value);
   document.getElementById("submit-upload").disabled = !allFilled;
+  if (document.querySelector(".add-status")) {
+    document.querySelector(".add-status").classList.remove("add-status");
+  }
 }
-
 inputs.forEach((e) => {
   e.addEventListener("change", checkInputs);
 });
 
+// Envoi les données du formulaire au backend
 document
   .getElementById("photo-upload-form")
   .addEventListener("submit", async (e) => {
@@ -240,9 +266,6 @@ document
       "category",
       inputs[2].selectedOptions[0].getAttribute("data-id")
     );
-    for (const [key, value] of formData.entries()) {
-      console.log(`${key}:`, value);
-    }
     const result = await fetchData(WORKSAPI, {
       method: "POST",
       headers: {
@@ -250,11 +273,11 @@ document
       },
       body: formData,
     });
+    // Si l'envoi fonctionne relance l'affichage des galleries et affiche un message à l'utilisateur pour lui confirmer l'envoi
     if (result) {
-      console.log(result);
-
-      displayGallery();
+      await initGallery();
       displayModal();
+      document.getElementById("add-success").classList.add("add-status");
       const preview = document.getElementById("image-preview");
       preview.src = "";
       preview.classList.remove("image-loaded");
@@ -264,5 +287,7 @@ document
       inputs.forEach((item) => {
         item.value = "";
       });
+    } else { // Affiche un message à l'utilisateur si l'envoi ne fonctionne pas
+      document.getElementById("add-echec").classList.add("add-status");
     }
   });
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,53 +1,56 @@
-const logsInput = {email : document.getElementById("email"), password : document.getElementById("password") }
+const logsInput = {
+  email: document.getElementById("email"),
+  password: document.getElementById("password"),
+};
 const textLogin = document.getElementById("login-text-error");
 let userToken = window.sessionStorage.getItem("token");
 
-if (userToken)  {
-    window.location.href = 'index.html';
+// Renvoi l'utilisateur à l'accueil si il est déjà connecté
+if (userToken) {
+  window.location.href = "index.html";
 }
 
+// Test les logs rentrés par l'utilisateur et le connecte si il possède un compte
 async function testLogin(user) {
-    
-    try {
-        const response = await fetch("http://localhost:5678/api/users/login", {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(user)
-        })
+  try {
+    const response = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    });
 
-        if (!response.ok) {
-            throw new Error (`Erreur HTTP : ${response.status} ${response.statusText}`)
-        }
-        
-        const data = await response.json();
-        window.sessionStorage.setItem("token", data.token);
-        window.location.href = 'index.html';
-        
-    } catch (error) {
-        Object.values(logsInput).forEach((input) => {
-            input.classList.add("login-error");
-            input.value = "";
-        })
-        textLogin.classList.add("login-error")
+    if (!response.ok) {
+      throw new Error(
+        `Erreur HTTP : ${response.status} ${response.statusText}`
+      );
     }
+    const data = await response.json();
+    window.sessionStorage.setItem("token", data.token);
+    window.location.href = "index.html";
+  } catch (error) {
+    Object.values(logsInput).forEach((input) => {
+      input.classList.add("login-error");
+      input.value = "";
+    });
+    textLogin.classList.add("login-error");
+  }
 }
 
+// Lance la fonction testLogin
 document.querySelector("#login-form form").addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const user = {
-        email : logsInput.email.value,
-        password : logsInput.password.value
-    }
-    testLogin(user);
-    
+  e.preventDefault();
+  const user = {
+    email: logsInput.email.value,
+    password: logsInput.password.value,
+  };
+  testLogin(user);
 });
 
 Object.values(logsInput).forEach((input) => {
-    input.addEventListener("click", () => {
-        Object.values(logsInput).forEach((element)=> {
-            element.classList.remove("login-error");
-        })
-        textLogin.classList.remove("login-error")
-    })
-})
+  input.addEventListener("click", () => {
+    Object.values(logsInput).forEach((element) => {
+      element.classList.remove("login-error");
+    });
+    textLogin.classList.remove("login-error");
+  });
+});
